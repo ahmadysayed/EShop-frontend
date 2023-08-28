feb6560646871c6b2a0eb6bf6bc2511deaec4e4c
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-08-28 11:16:54 +0200

    Admin List Product

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -31,7 +31,7 @@ function UserListScreen() {
         }else {
             history('/login')
         }
-    }, [dispatch, history, successDelete])
+    }, [dispatch, history, successDelete, userInfo])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure you want to delete this user?')){
